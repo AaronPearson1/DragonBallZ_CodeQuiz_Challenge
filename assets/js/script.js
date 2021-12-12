beadf9a102053728a96d5e0f413930197f9805a6
Aaron Pearson <pearsonaaron.56@gmail.com>
2021-12-12 14:02:28 -0500

    Initial CSS

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+// Try to answer the following code-related questions within the time limtit. Keep in mind that incorrect answers will penalize your score/time by 10 seconds!
+
+// Commonly used data types Do Not Incldue:
+// 1. Strings
+// 2. Booleans
+// 3. Alerts
+// 4. Numbers
+
+// The condition in an if/ else statement is enclosed with ________.
+// 1. Quotes
+// 2. Curly Brackets
+// 3. Parenthesis
+// 4. Square Brackets
+
+// Arrays inJavaScript can be used to store _________. 
+// 1. Numbers and Strings
+// 2. Other Arrays
+// 3. Booleans
+// 4. All of the Above
+
+// String values must be enclosed within _______ when being asigned to variables. 
+// 1. Commas
+// 2. Curly Brackets
+// 3. Quotes
+// 4. Parenthesis
+
+// A very useful tool used during developmenet and debugging for printing content to the debugger is:
+// 1. JavaScript
+// 2. Terminal/Bash
+// 3. For Loops
+// 4. Console.log
+
